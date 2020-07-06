8a42df0f5556e8f413085afdfabb4c9c5e134f03
MahimaManuela <59163075+MahimaManuela@users.noreply.github.com>
2020-07-06 21:50:47 +0530

    Code for first video: Variable

---
+++ variable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//addition of 2 two numbers
+
+int main(){
+   int	a=3, b=4; //definition of variables
+   float sum;
+   sum=a+b;
+   cout<<sum;
+   return 0;
+}
