a3dd0fc24f16499c216e591293834599a0d89b94
MahimaManuela <59163075+MahimaManuela@users.noreply.github.com>
2020-07-15 22:25:41 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,14 @@
+//Basic Comparisons between C and C++
+
+#include<stdio.h>
+
+
+int main(){
+   int m = 3;
+   float n = 5.1;
+   
+   printf("%f", m*n); 
+   
+   /*This is a 
+       multiple line comment */
+}
