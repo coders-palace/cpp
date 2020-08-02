091b9ea57bbe75f34301fa6396109afa31e0a514
MahimaManuela <59163075+MahimaManuela@users.noreply.github.com>
2020-08-02 22:39:08 +0530

    Add files via upload

---
+++ Loops.cpp
@@ -0,0 +1,15 @@
+//Loops - avoid writing a set of similar statements
+
+#include<iostream>
+using namespace std;
+
+int main(){ 
+       
+	//the for loop
+	for(int x=1 ; x<=10 ;x++ ){ //x = 1;
+	                         
+		cout<< x <<endl; // x<=10
+	}
+	
+	
+	}
