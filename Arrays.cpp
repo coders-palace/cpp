33da1474b0cb153435079583e1a9a0167d0d5acd
MahimaManuela <59163075+MahimaManuela@users.noreply.github.com>
2020-08-09 22:41:39 +0530

    Add files via upload

---
+++ Arrays.cpp
@@ -0,0 +1,19 @@
+//Arrays - group of similar data
+//same data type
+//a[0],a[1],a[2]
+
+#include<iostream>
+using namespace std;
+
+int main(){
+        int a[10];//index of an array - number of elements 
+        
+        //for loop
+        for(int i=0; i<10;i++){
+        	cin>>a[i];
+		}
+		
+		for(int i=0; i<10;i++){
+        	cout<<a[i]<<endl;
+		}
+       }
