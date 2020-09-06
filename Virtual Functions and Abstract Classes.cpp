740df3ec9ff04bf6b1f76d8135faf22bdfa60531
MahimaManuela <59163075+MahimaManuela@users.noreply.github.com>
2020-09-06 22:12:15 +0530

    Add files via upload

---
+++ Virtual Functions and Abstract Classes.cpp
@@ -0,0 +1,26 @@
+//Virtual Functions and Abstract Classes
+#include<iostream>
+using namespace std;
+
+class base//abstract class
+{
+	public:
+		virtual void display(){
+			cout<<"Base";
+		}
+};
+
+class derived:public base{
+	
+	public:
+		void display(){
+			cout<<"Derived";
+		}
+};
+
+int main(){
+  base* b;//base pointer
+  derived d;
+  b= &d ;
+  b->display();//Late Binding
+}
